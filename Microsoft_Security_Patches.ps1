32e558e4fc00650f59f9523951b43659aadce6ad
eggstoastbacon <55957368+eggstoastbacon@users.noreply.github.com>
2019-11-16 21:11:32 -0800

    Update Microsoft_Security_Patches.ps1

--- Microsoft_Security_Patches.ps1
+++ Microsoft_Security_Patches.ps1
@@ -2,6 +2,7 @@
 #Downloads the currents month's security updates and servicing stack updates. 
 #Includes, IE 11, .NET, Servicing Stack, Security Only and Monthly Rollup.
 #Compares file size headers and re-downloads corrupt files.
+#Downloads the patch description to am individual text file.
 
 #OS to download
 $OSList = "Server+2012+R2","Server+2008+R2","Server+2016"
